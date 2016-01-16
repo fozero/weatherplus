efb9dfc064f249d7f67bffbe139722161fecd3ab
fozero <fozero@126.com>
2016-01-16 20:26:16 +0800

    实现天气信息显示.

--- src/com/weatherplus/app/activity/ChooseAreaActivity.java
+++ src/com/weatherplus/app/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.weatherplus.app.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -69,6 +72,15 @@ public class ChooseAreaActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		
+		//获取本地存储的数据
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		//获取控件实例
 		listView = (ListView) findViewById(R.id.list_view);
 		titleText = (TextView) findViewById(R.id.title_text);
@@ -84,7 +96,6 @@ public class ChooseAreaActivity extends Activity {
 		
 		//设置ListView点击事件
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				if (currentLevel == LEVEL_PROVINCE) {
@@ -93,10 +104,14 @@ public class ChooseAreaActivity extends Activity {
 				} else if (currentLevel == LEVEL_CITY) {
 					selectedCity = cityList.get(position);
 					queryCounties();
+				}else if (currentLevel == LEVEL_COUNTY) {//县级
+					String countyCode = countyList.get(position).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
-			
-			
 		});
 		
 		
